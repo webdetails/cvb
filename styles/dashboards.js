71dd68295788dd87485e590157152c96f4c8f64d
Goncalo Neto <goncalo.neto@webdetails.pt>
2013-03-25 11:48:07 +0000

    Add expanding row placeholder.

---
+++ styles/dashboards.js
@@ -0,0 +1,17 @@
+[
+			{
+				"name": "Initial Test",
+				"id": "initialTest.wcdf",
+				"link": "/pentaho/content/cvb/initialtest"
+			},
+			{
+				"name": "Second Test",
+				"id": "secondTest.wcdf",
+				"link": "/pentaho/content/cvb/secondtest"
+			},
+			{
+				"name": "File List",
+				"id": "fileList.wcdf",
+				"link": "/pentaho/content/cvb/filelist"
+			}
+]
